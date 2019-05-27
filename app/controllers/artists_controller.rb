414208200f47c3ffa734a0478796bab0268affc0
Joy Lin <vvlnote@hotmail.com>
2019-05-27 20:52:08 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,12 @@
 class ArtistsController < ApplicationController
+
+
   def index
+    @artists = Artist.all
   end
 
   def show
+    @artist = find_artrist!
   end
 
   def new
@@ -46,5 +50,9 @@ class ArtistsController < ApplicationController
 
   def artist_params
     params.require(:artist).permit(:name)
+  end
+
+  def find_artrist!
+    Artist.find(params[:id])
   end
 end
